ae01dce4cb64a8565067f9f814b32be26bd7f261
Bradley Allen <bradley.allen84@gmail.com>
2016-01-18 20:21:35 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(countFrom)
+	idx = countFrom
+	while idx > 0
+		puts "#{idx} SECOND(S)!"
+		idx -= 1
+	end
+	return "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(countFrom)
+	idx = countFrom
+	while idx > 0
+		puts "#{idx} SECOND(S)!"
+		idx -= 1
+		sleep(1)
+	end
+	return "HAPPY NEW YEAR!"
 end
